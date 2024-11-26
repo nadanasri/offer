b367b113a0a520800ffe6dc7305e8db23ec9c30e
Nadanasri11 <nadanasrirk2020@psnacet.edu.in>
2024-11-26 15:05:17 +0530

    Updated API

--- src/API's/Api.js
+++ src/API's/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const API = axios.create({
+export const Api = axios.create({
   baseURL: 'http://localhost:3004', // Base server URL
 });
 
-export default API;
+export default Api;
