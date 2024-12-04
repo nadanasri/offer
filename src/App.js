4793989c161541d182e730c33121bb6379c5f757
Nadana Sri <11nadanasri@gmail.com>
2024-12-04 15:44:55 +0530

    conflict

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css'
-import Navigationbar from './components/NavigationBar';
+import Navbar from './components/NavigationBar';
 import Dashboard from './pages/Dashboard';
 import ContactUs from './pages/ContactUs';
 import AppDemo from './pages/AppDemo';
 import Offers from './pages/Offers';
 
+
+
+
 const App = () => {
     return (
         <BrowserRouter>
             <div>
-                <Navigationbar />
+                <Navbar />
                 <Routes>
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/contact" element={<ContactUs />} />
-                    <Route path="/offers" element={<Offers />} />
+                    <Route path="/offers" element={<Offers/>} />
                     <Route path="/" element={<Dashboard />} />  
                     <Route path="/demo" element={<AppDemo />} />
                 </Routes>
